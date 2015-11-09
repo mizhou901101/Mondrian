aa36544917f4f54bcc936d647f41e8af79834621
willy901101@gmail.com <willy901101@gmai.com>
2015-11-09 16:10:48 +1300

    tidy the code up

--- demo/demo-core/src/main/java/org/demo/core/manage/UserManage.java
+++
@@ -1,8 +0,0 @@
-package org.demo.core.manage;
-
-import org.demo.common.jodo.UserDO;
-
-public interface UserManage {
-	
-	public UserDO queryUser(Integer id);
-}
--- demo/demo-core/src/main/java/org/demo/core/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.demo.core.service;
-
-import org.demo.common.jodo.UserDO;
-
-public interface UserService {
-	public UserDO queryUser(Integer id);
-}
